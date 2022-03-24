8daaf0c4f5d5f336be60769a1931c5e60475c837
TraxxasWyls <savinov.dsta@gmail.com>
2022-03-24 14:11:38 +0300

    Documentation added

--- Sources/CombineExtensions/AnyPublisher.swift
+++ Sources/CombineExtensions/AnyPublisher.swift
@@ -31,6 +31,9 @@ extension AnyPublisher {
         AnyPublisher(factory)
     }
 
+    /// A publisher that awaits subscription before running future to create a publisher for the new subscriber.
+    /// - Parameter attemptToFulfill: target fulfil
+    /// - Returns: Defered publisher with future wraped with a type eraser.
     public static func deferred(
         _ attemptToFulfill: @escaping (@escaping Future<Output, Failure>.Promise) -> Void
     ) -> AnyPublisher {
@@ -39,12 +42,18 @@ extension AnyPublisher {
         }.eraseToAnyPublisher()
     }
 
+    /// A publisher that eventually produces a single value and then finishes or fails.
+    /// - Parameter attemptToFulfill: target fulfil
+    /// - Returns: Future publisher wraped with a type eraser.
     public static func future(
         _ attemptToFulfill: @escaping (@escaping Future<Output, Failure>.Promise) -> Void
     ) -> AnyPublisher {
         Future(attemptToFulfill).eraseToAnyPublisher()
     }
 
+    /// A publisher that immediatly emits an output to each subscriber just once, and then finishes.
+    /// - Parameter work: target work to execute
+    /// - Returns: Just publisher wraped with a type eraser.
     public static func immediate(_ work: () -> Void) -> AnyPublisher {
         work()
         return Just<Output?>(nil)
@@ -53,6 +62,9 @@ extension AnyPublisher {
             .eraseToAnyPublisher()
     }
 
+    /// A publisher created by applying the merge function to an arbitrary number of upstream publishers.
+    /// - Parameter publishers: target publishers for merge
+    /// - Returns: MergeMany publisher wraped with a type eraser.
     public static func merge(_ publishers: [AnyPublisher]) -> AnyPublisher {
         Publishers
             .MergeMany(publishers)
@@ -64,6 +76,10 @@ extension AnyPublisher {
 
 extension AnyPublisher where Failure == NSError {
 
+    /// A publisher that immediatly emits an output to each subscriber just once, and then finishes.
+    /// Used when Failure is NSError and returns Fail publisher when error is catched.
+    /// - Parameter work: target work to execute
+    /// - Returns: Just or Fail publisher wraped with a type eraser.
     public static func immediate(_ work: () throws -> Void) -> AnyPublisher {
         do {
             try work()
@@ -74,13 +90,5 @@ extension AnyPublisher where Failure == NSError {
         } catch {
             return Fail(error: error as NSError).eraseToAnyPublisher()
         }
-    }
-
-    public static func immediate(_ work: () -> Void) -> AnyPublisher {
-        work()
-        return Just<Output?>(nil)
-            .setFailureType(to: Failure.self)
-            .compactMap { $0 }
-            .eraseToAnyPublisher()
     }
 }
--- Sources/CombineExtensions/Publisher.swift
+++ Sources/CombineExtensions/Publisher.swift
@@ -10,6 +10,9 @@ import Combine
 // MARK: - Publisher
 
 extension Publisher where Output == Never {
+
+    /// Converts publisher to empty Just publisher and erases to AnyPublisher
+    /// - Returns: Void AnyPublisher
     public func asVoid() -> AnyPublisher<Void, Failure> {
         self
             .map { _ in }
@@ -19,6 +22,10 @@ extension Publisher where Output == Never {
 }
 
 extension Publisher where Output == Void {
+
+    /// Ignores all upstream elements, but passes along the upstream publisher’s completion state (finished or failed).
+    /// - Parameter completeImmediately: if true - immediately finishes, otherwise just ignores output.
+    /// - Returns: Type erased AnyPublisher with ignored output.
     public func asNever(completeImmediately: Bool) -> AnyPublisher<Never, Failure> {
         if completeImmediately {
             return Just<Void>(())
